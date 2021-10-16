eb1150fb8d9425fb34e60f4bc672dbe578047a8d
Musa17 <amusul00@fesb.hr>
2021-10-16 12:33:39 +0200

    Adding variables

--- Snake Game WF/Form1.cs
+++ Snake Game WF/Form1.cs
@@ -12,6 +12,15 @@ namespace Snake_Game_WF
 {
     public partial class Form1 : Form
     {
+        int cols = 50, rows = 25, score = 0, dx = 0, dy = 0, front = 0, back = 0;
+        Piece[] snake = new Piece[1250];
+        List<int> available = new List<int>();
+        bool[,] visit;
+
+        Random rand = new Random();
+        Timer timer = new Timer();
+
+
         public Form1()
         {
             InitializeComponent();
